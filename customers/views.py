dd3f2b3883c5db7401af05d8e7a51a21f7497c76
Ak Zilani <jilanichy57@gmail.com>
2022-08-17 23:32:53 +0600

    Again add commented section of views

--- customers/views.py
+++ customers/views.py
@@ -44,3 +44,45 @@ class SubscriptionDetail(generics.RetrieveUpdateDestroyAPIView):
 
 
 
+# @api_view(['GET', 'POST'])
+# def subscription_list(request, format=None):
+#     """
+#     List all subscriptions, or create a new subscription.
+#     """
+#     if request.method == 'GET':
+#         subscriptions = SubscriptionInfo.objects.all()
+#         serializer = SubscriptionInfoSerializer(subscriptions, many=True)
+#         return Response(serializer.data)
+
+#     elif request.method == 'POST':
+#         serializer = SubscriptionInfoSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_201_CREATED)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+# @api_view(['GET', 'PUT', 'DELETE'])
+# def subscription_detail(request, pk, format=None):
+#     """
+#     Retrieve, update or delete a subscription
+#     """
+#     try:
+#         subscriber = SubscriptionInfo.objects.get(pk=pk)
+#     except SubscriptionInfo.DoesNotExist:
+#         return Response(status=status.HTTP_404_NOT_FOUND)
+
+#     if request.method == 'GET':
+#         serializer = SubscriptionInfoSerializer(subscriber)
+#         return Response(serializer.data)
+
+#     elif request.method == 'PUT':
+#         serializer = SubscriptionInfoSerializer(subscriber, data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+#     elif request.method == 'DELETE':
+#         subscriber.delete()
+#         return Response(status=status.HTTP_204_NO_CONTENT)
